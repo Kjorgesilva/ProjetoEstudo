86679afba1eadfec842985ba71e6f7f190186e95
Kjorgesilva <kaiqueadm@hotmail.com>
2021-11-10 00:05:24 -0300

    Segundo commit correção

--- app/src/main/java/com/example/projetoestudo/AdapterItem.kt
+++ app/src/main/java/com/example/projetoestudo/AdapterItem.kt
@@ -7,25 +7,25 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterItem(val listaItem: ArrayList<Livro>,val context: Context): RecyclerView.Adapter<AdapterItem.itemViewHolder>(){
+class AdapterItem(val listaItem: ArrayList<Livro>,val context: Context): RecyclerView.Adapter<AdapterItem.ItemViewHolder>(){
 
 
-    class itemViewHolder(view: View): RecyclerView.ViewHolder(view){
+    inner class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
         val titulo = view.findViewById(R.id.txtTitulo) as TextView
         val data = view.findViewById(R.id.txt_data_publicacao) as TextView
         val autor = view.findViewById(R.id.txt_autor) as TextView
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): itemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         //parent?.context... varifica se o contexto esta null
-        val viewLayoutInfla = LayoutInflater.from(parent?.context).inflate(R.layout.adapter_item,parent,false)
-        return itemViewHolder(viewLayoutInfla)
+        val viewLayoutInfla = LayoutInflater.from(parent.context).inflate(R.layout.adapter_item,parent,false)
+        return ItemViewHolder(viewLayoutInfla)
     }
 
-    override fun onBindViewHolder(holder: itemViewHolder, position: Int) {
-        holder.titulo?.text = listaItem[position].titulo
-        holder.data?.text = listaItem[position].dataPublicacao
-        holder.autor?.text = listaItem[position].autor
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.titulo.text = listaItem[position].titulo
+        holder.data.text = listaItem[position].dataPublicacao
+        holder.autor.text = listaItem[position].autor
     }
 
     override fun getItemCount(): Int = listaItem.size
--- app/src/main/java/com/example/projetoestudo/Livro.kt
+++ app/src/main/java/com/example/projetoestudo/Livro.kt
@@ -1,13 +1,4 @@
 package com.example.projetoestudo
 
-class Livro {
-    var titulo:String
-    var dataPublicacao: String
-    var autor: String
-
-    constructor(titulo: String, dataPublicacao: String, autor: String) {
-        this.titulo = titulo
-        this.dataPublicacao = dataPublicacao
-        this.autor = autor
-    }
+data class Livro (var titulo:String, var dataPublicacao: String, var autor: String){
 }
